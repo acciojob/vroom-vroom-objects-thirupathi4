53217020b4960a613ffa31ad2571d66fe5c625d3
Thirupathi Rao <162027483+thirupathi4@users.noreply.github.com>
2025-09-06 12:34:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	constructor(make,model);
+	this.make=make;
+	this.model=model;
+	getMakeModel(){
+		return `${make} ${model}`;
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar extends Car(make, model, topSpeed) {
+	constructor(make,model,topSpeed);
+	this.topSpeed=topSpeed;
+	getTopSpeed(){
+		return `${topSpeed}`;
+	}
+	
+}
 
 // Do not change the code below
 window.Car = Car;
