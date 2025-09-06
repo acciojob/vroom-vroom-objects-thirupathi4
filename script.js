fd5d841b20e6a50a8b92dadfa44c0e7a82034feb
Thirupathi Rao <162027483+thirupathi4@users.noreply.github.com>
2025-09-06 12:36:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,6 +16,9 @@ function SportsCar extends Car(make, model, topSpeed) {
 	}
 	
 }
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
 
 // Do not change the code below
 window.Car = Car;
